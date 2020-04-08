7dc4b255d68f8429b16e193432600962876b9a8f
unknown <xxoliwerxx@gmail.com>
2020-04-08 08:09:29 +0200

    initial commit

---
+++ src/main/java/magicsquared/util.java
@@ -0,0 +1,11 @@
+package magicsquared;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class util {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	public static final String MODID = "magicsquared";
+}
