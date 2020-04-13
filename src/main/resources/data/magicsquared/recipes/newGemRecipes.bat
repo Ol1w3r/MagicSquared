ebfa79abe0e418db3a071c5f9e33a36050186987
OliW3r <xxoliwerxx@gmail.com>
2020-04-13 10:30:45 +0200

    recipes

---
+++ src/main/resources/data/magicsquared/recipes/newGemRecipes.bat
@@ -0,0 +1,7 @@
+set /p name="Gem type: "
+copy amethystaxe.json %name%axe.json
+copy amethystpickaxe.json %name%pickaxe.json
+copy amethystshovel.json %name%shovel.json
+copy amethystsword.json %name%sword.json
+copy amethystblock.json %name%block.json
+copy amethystitem.json %name%item.json
